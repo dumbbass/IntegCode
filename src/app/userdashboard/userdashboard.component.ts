302e3558463f601d77aad805807dcb1c6ebdf120
dumbbass <nedrudakeanuleo27@Gmail.com>
2025-01-11 12:54:40 +0800

    Box

--- src/app/userdashboard/userdashboard.component.ts
+++ src/app/userdashboard/userdashboard.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-userdashboard',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [CommonModule, SidenavComponent],
   templateUrl: './userdashboard.component.html',
   styleUrls: ['./userdashboard.component.css']
 })
 export class UserdashboardComponent {
   selectedDescription: string | null = null;
+  selectedDoctor: string | null = null;
+  selectedDate: string | null = null;
 
-  openDescription(description: string) {
+  openDescription(description: string, doctor: string = '', date: string = '') {
     this.selectedDescription = description;
+    this.selectedDoctor = doctor;
+    this.selectedDate = date;
   }
 
   closeDescription() {
     this.selectedDescription = null;
+    this.selectedDoctor = null;
+    this.selectedDate = null;
   }
 }
