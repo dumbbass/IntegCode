30ade747b02dd143a985351c8c4387002ef055b6
jeffryloveschinie <202211473@gordoncollege.edu.ph>
2025-01-27 01:15:10 -1200

    ugh

--- src/app/ADMIN/schedule/schedule.component.ts
+++ src/app/ADMIN/schedule/schedule.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AdminsidenavComponent } from '../adminsidenav/adminsidenav.component';
+import { startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays, isSameDay } from 'date-fns';
 
 @Component({
   selector: 'app-schedule',
@@ -15,4 +16,48 @@ import { AdminsidenavComponent } from '../adminsidenav/adminsidenav.component';
   styleUrl: './schedule.component.css'
 })
 export class ScheduleComponent {
+  currentMonth: Date = new Date();
+  selectedDates: Date[] = [];
+  weekDays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  daysInMonth: Date[] = [];
+
+  constructor() {
+    this.generateCalendar();
+  }
+
+  prevMonth() {
+    this.currentMonth = new Date(this.currentMonth.setMonth(this.currentMonth.getMonth() - 1));
+    this.generateCalendar();
+  }
+
+  nextMonth() {
+    this.currentMonth = new Date(this.currentMonth.setMonth(this.currentMonth.getMonth() + 1));
+    this.generateCalendar();
+  }
+
+  generateCalendar() {
+    const start = startOfWeek(startOfMonth(this.currentMonth));
+    const end = endOfWeek(endOfMonth(this.currentMonth));
+
+    const days = [];
+    let day = start;
+    while (day <= end) {
+      days.push(new Date(day));
+      day = addDays(day, 1);
+    }
+    this.daysInMonth = days;
+  }
+
+  toggleDate(date: Date) {
+    const index = this.selectedDates.findIndex((d) => isSameDay(d, date));
+    if (index > -1) {
+      this.selectedDates.splice(index, 1);
+    } else {
+      this.selectedDates.push(date);
+    }
+  }
+
+  isSelected(date: Date): boolean {
+    return this.selectedDates.some((d) => isSameDay(d, date));
+  }
 }
